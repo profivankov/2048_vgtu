b1836d8aef9828f9b4a693e26808fda00d7a5ab0
BonBon <BonBon@DESKTOP-15RAB9Q>
2019-11-11 02:53:16 +0200

    added two main services, left and right key methods

---
+++ 2048_vgtu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2048_vgtu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
